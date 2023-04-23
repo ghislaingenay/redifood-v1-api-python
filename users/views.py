24b6e075b46ffc15fcbe68d12ab0d7ff7ac45823
ghislaingenay <ghislain.gny@gmail.com>
2023-04-23 13:14:46 +0700

    Fix .InconsistentMigrationHistory: + minor issued for running server

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
-from serializers import UserSerializer
+from .serializers import UserSerializer
 from .models import User
 from .password_manager import PasswordManager
 import jwt, datetime
