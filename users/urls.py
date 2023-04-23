ea2e0a94e5294a9ab4e584ee900d8107883a70b0
ghislaingenay <ghislain.gny@gmail.com>
2023-04-23 20:25:31 +0700

    Change import utlpatterns for auth app

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,8 @@ from .views import RegisterView, LoginView, CurrentUserView, LogoutView
 from django.urls import path
 
 urlpatterns = [
-  path('register/', RegisterView.as_view(),name='register' ),
-  path('login/', LoginView.as_view(),name='login' ),
-  path('currentuser/', CurrentUserView.as_view(),name='currentuser' ),
-  path('logout/', LogoutView.as_view(),name='logout'),
+  path('register', RegisterView.as_view(),name='register' ),
+  path('login', LoginView.as_view(),name='login' ),
+  path('currentuser', CurrentUserView.as_view(),name='currentuser' ),
+  path('logout', LogoutView.as_view(),name='logout'),
 ]
