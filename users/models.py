3cea3d6be6f4158806215e7a31f58a2ed0ac67f9
ghislaingenay <ghislain.gny@gmail.com>
2023-04-23 16:14:16 +0700

    Possibility to have username NULL

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
   full_name = models.CharField(max_length=40, null=True)
   email = models.EmailField(max_length=254, unique=True)
-  username = models.CharField(max_length=40, unique=True)
+  username = models.CharField(max_length=40, unique=True, null=True)
   password = models.CharField(max_length=40)
   created_at = models.DateTimeField(auto_now_add=True)
   
