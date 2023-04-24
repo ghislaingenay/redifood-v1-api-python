3c755155b55b0a2a82b34479b38f7fa412f2e4c5
ghislaingenay <ghislain.gny@gmail.com>
2023-04-24 21:56:51 +0700

    P2 Create backbone for foods app

--- food_pos_api/settings.py
+++ food_pos_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'users.apps.UsersConfig',
+    'foods.apps.FoodsConfig',
     "corsheaders",
     'rest_framework',
 ]
--- food_pos_api/urls.py
+++ food_pos_api/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('users.urls'), name='auth')
+    path('api/auth/', include('users.urls'), name='auth'),
+    path('api/foods/', include('foods.urls'), name='foods'),
 ]
