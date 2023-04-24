8932f8dfced7194a086a0409dacb908b1ba310fe
ghislaingenay <ghislain.gny@gmail.com>
2023-04-24 22:27:03 +0700

    P2 Migrate Food model to database

--- foods/models.py
+++ foods/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Food(models.Model):
-    food_name = models.CharField(max_length=100,null=False, required=True)
-    food_description = models.TextField(max_length=500, blank=True, required=False)
+    food_name = models.CharField(max_length=100,null=False, unique=True)
+    food_description = models.TextField(max_length=500, blank=True)
     food_price = models.DecimalField(max_digits=5, decimal_places=2)
-    image_url = models.CharField(max_length=200, blank=True, required=False)
-    food_category = models.CharField(max_length=100, blank=True, required=False)
+    image_url = models.CharField(max_length=200, blank=True)
+    food_category = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.food_name[:50]
