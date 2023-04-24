2521731a028497dd0d2b9a5532df31f9d6ddd081
ghislaingenay <ghislain.gny@gmail.com>
2023-04-24 22:16:41 +0700

    P2 Implement basic Food Model

--- foods/models.py
+++ foods/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+def 
+
 # Create your models here.
+class Food(models.Model):
+    food_name = models.CharField(max_length=100,null=False, required=True)
+    food_description = models.TextField(max_length=500, blank=True, required=False)
+    food_price = models.DecimalField(max_digits=5, decimal_places=2)
+    image_url = models.CharField(max_length=200, blank=True, required=False)
+    food_category = models.CharField(max_length=100, blank=True, required=False)
+
+    def __str__(self):
+        return self.food_name[:50]
+      
+    
