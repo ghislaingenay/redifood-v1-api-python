7ad17624fa83fa01f413b97be9fff4f05b3c1797
ghislaingenay <ghislain.gny@gmail.com>
2023-04-25 08:38:38 +0700

    P2 Add view to get all foods

--- foods/views.py
+++ foods/views.py
@@ -1,11 +1,13 @@
 from django.db import connection
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from users.auth_decorators import is_authenticated
 
 # Create your views here. 
 class GetFoodsView(APIView):
-    def get(self, request):
-      with connection.cursor() as cursor:
-        cursor.execute('SELECT * FROM foods_food')
-        rows = cursor.fetchall()
-        return Response(rows)
+  def get(self, request):
+    print('user', request.user)
+    with connection.cursor() as cursor:
+      cursor.execute('SELECT * FROM foods_food')
+      rows = cursor.fetchall()
+      return Response(rows)
