8d4c3d66eb947f568fad11e61b9035f9779ad334
ghislaingenay <ghislain.gny@gmail.com>
2023-04-26 21:08:20 +0700

    P2 Crete food POST

--- foods/views.py
+++ foods/views.py
@@ -1,6 +1,10 @@
 from django.db import connection
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from operator import itemgetter
+from foods.cloudinary import Cloudinary
+
+from foods.serializer import FoodSerializer
 
 # Create your views here. 
 class GetFoodsView(APIView):
@@ -23,3 +27,17 @@ class FoodIdView(APIView):
     with connection.cursor() as cursor:
       cursor.execute(f'DELETE FROM foods_food WHERE id = {food_id}')
       return Response({'message': 'Food deleted'})
+  
+  def put(self, request, food_id):
+    serializer = FoodSerializer(data=request.data)
+    if serializer.is_valid():    
+      food_name, food_description, food_price, image_url, food_category = itemgetter('food_name', 'food_description', 'food_price', 'image_url', 'food_category', 'base64_image')(serializer.validated_data)
+      with connection.cursor() as cursor:
+        cursor.execute(f'UPDATE foods_food SET food_name = "{food_name}", food_description = "{food_description}", food_price = {food_price}, image_url = "{image_url}", food_category = "{food_category}", base64_image="{base64_image}" WHERE id = {food_id}')
+        return Response({'message': 'Food updated'})
+      
+  def post(self, request):
+    serializer = FoodSerializer(data=request.data) # Create in FoodSerializer create a url link directly
+    if serializer.is_valid():
+      serializer.save()
+    
