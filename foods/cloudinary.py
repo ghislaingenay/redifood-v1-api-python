8ec33a51ad3288c46f9ac36276bbc850bc1fd0da
ghislaingenay <ghislain.gny@gmail.com>
2023-04-26 20:50:17 +0700

    P2 Change cloiudinary class to upload file

--- foods/cloudinary.py
+++ foods/cloudinary.py
@@ -1,7 +1,17 @@
 import base64
 from django.core.files.base import ContentFile
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+import os
 
 class Cloudinary:
+  cloudinary.config( 
+  cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME'),
+  api_key = os.getenv('CLOUDINARY_API_KEY'), 
+  api_secret = os.getenv('CLOUDINARY_API_SECRET'),
+  secure = True
+)
   
 
   def decode_base64_image(data: str) -> ContentFile:
@@ -12,8 +22,9 @@ class Cloudinary:
     ext = format.split('/')[-1] 
     data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext) 
 
-  def save_image_to_cloudinary(data: str) -> str:
+  def save_image_to_cloudinary(base64_file: str) -> str:
     '''
     Function that saves an image to a cloud provider
     '''
-    pass
+    created_file = cloudinary.uploader.upload(base64_file)
+    return created_file
