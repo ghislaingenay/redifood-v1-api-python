82d6c4a774706055e3fbe500f5a3a7fff8c78b13
ghislaingenay <ghislain.gny@gmail.com>
2023-04-26 20:49:52 +0700

    P2 Add base64_email field to model

--- foods/models.py
+++ foods/models.py
@@ -7,6 +7,7 @@ class Food(models.Model):
     food_price = models.DecimalField(max_digits=5, decimal_places=2)
     image_url = models.CharField(max_length=200, blank=True)
     food_category = models.CharField(max_length=100, blank=True)
+    base64_image = models.CharField(max_length=100000, blank=True)
 
     def __str__(self):
         return self.food_name[:50]
