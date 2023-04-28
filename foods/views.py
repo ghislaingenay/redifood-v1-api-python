1e95c7d0109089dc91343317cb91e6581ba685ee
ghislaingenay <ghislain.gny@gmail.com>
2023-04-28 15:12:20 +0700

    P3 Fix file naming for serailizer in foods

--- foods/views.py
+++ foods/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from operator import itemgetter
 from foods.cloudinary import Cloudinary
 
-from foods.serializer import FoodSerializer
+from foods.serializers import FoodSerializer
 
 # Create your views here. 
 class GetFoodsView(APIView):
